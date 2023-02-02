57a21cdc3ca5669ed9e374d0ebef067357de0c35
mislant <kiryakz2000@gmail.com>
2023-02-02 21:52:44 +0600

    cosmetic: pretty recipe blocks

--- app/views/site/index.php
+++ app/views/site/index.php
@@ -4,35 +4,40 @@
  * @var Recipe[] $recipes
  */
 
+use Kitman\Application\Query\Recipe\Recipe;
 use Kitman\Infrastructure\Web\Helper\Url;
 use yii\bootstrap5\Html;
 use yii\web\View;
-use Kitman\Application\Query\Recipe\Recipe;
 
 ?>
 
 <div class="container">
     <h1 class="fw-light mb-2"><?= "List of available recipes" ?></h1>
-    <div class="row row-cols gy-3">
-        <?php foreach ($recipes as $recipe): ?>
-            <div class="col-12 col-sm-6 col-md-6 col-lg-3">
-                <div class="rounded-1 border-5 border-start border-primary
-                            d-flex bg-white shadow ps-3 h-100">
-                    <div class="w-100 py-2 d-flex flex-column">
-                        <h5 class="text-primary border-1 border-bottom">
-                            <?= $recipe->name ?> <?= $recipe->calories ?>cal
-                        </h5>
-                        <span>
-                            <?= $recipe->description ?>
-                        </span>
+    <div class="d-flex flex-wrap justify-content-start">
+        <?php foreach ($recipes as $index => $recipe): ?>
+            <div class="my-2 me-3 row g-0 rounded-1 border-5 border-start border-primary bg-white shadow ps-3"
+                 style="width: 390px; max-height: 300px">
+                <div class="col-8 d-flex flex-column">
+                    <div class="d-flex pt-2 mb-2 border-1 border-bottom">
+                        <h5 class="text-primary" style="width: 90%"><?= $recipe->name ?></h5>
+                        <div class="dropdown dropstart">
+                            <a class="fs-5" type="button" id="recipeDropDown<?= $index ?>"
+                               data-bs-toggle="dropdown" aria-expanded="false">
+                                <i class="bi bi-sliders"></i>
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="recipeDropDown<?= $index ?>">
+                                <li><?= Html::a('Edit', Url::editRecipe($recipe->uuid), ['class' => 'dropdown-item']
+                                    ) ?></li>
+                            </ul>
+                        </div>
                     </div>
-                    <div class="border-1 border-start d-flex align-items-center justify-content-end px-3">
-                        <?= Html::a(
-                            '<i class="bi bi-pencil"></i>',
-                            Url::editRecipe($recipe->uuid),
-                            ['class' => 'text-decoration-none']
-                        ) ?>
-                    </div>
+                    <p class="fs-6 fw-light">Calories: <?= $recipe->calories ?></p>
+                    <p class="d-inline-block text-wrap" style=""><?= $recipe->description ?></p>
+                </div>
+                <div class="col-4 border-1 overflow-hidden">
+                    <img src="https://via.placeholder.com/300x200"
+                         class="p-1 w-100 h-100" alt="Dish Image"
+                         style="object-fit: cover">
                 </div>
             </div>
         <?php endforeach; ?>
